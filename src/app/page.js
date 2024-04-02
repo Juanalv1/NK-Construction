e025a614f13b3e6419697180c3980aa43e342635
Juan <elquesififa@gmail.com>
2024-04-02 09:43:58 -0500

    added images

--- src/app/page.js
+++ src/app/page.js
@@ -3,15 +3,14 @@ import InfoCard1 from './components/InfoCard1'
 import InfoCard2 from './components/InfoCard2'
 import InfoCard3 from './components/InfoCard3'
 import Footer from './components/Footer'
-import { MdFacebook } from 'react-icons/md'
-import { LuFacebook, LuInstagram } from 'react-icons/lu'
-import { IoLogoFacebook } from 'react-icons/io5'
+import { Carousel } from "flowbite-react";
 export const metadata = {
   title: 'Nk construction | Concrete pumping service',
   description: 'Nk construction provides best concrete pumping service in Dallas',
 }
 
 export default function Home() {
+
   return (
     <main className="flex min-h-screen flex-col">
       <section className='flex relative bg-stone-100 py-6 lg:py-12 lg:items-center lg:px-16 flex-col lg:flex-row'>
@@ -26,7 +25,7 @@ export default function Home() {
                   <button className='bg-red-500 p-2 rounded-lg  hover:shadow-lg hover:border-dotted text-white my-2'>Free estimate</button>
 
         </div>
-        <a href="https://storyset.com/work" className='flex lg:w-1/2 justify-center items-center'><img src='/Construction-rafiki.svg' className='flex w-96 h-96'/></a>
+        <a href="https://storyset.com/work" className='flex lg:w-1/2 justify-center items-center'><img src='/Construction-rafiki.svg' className='flex lg:w-[450px] lg:h-[450px] h-96 w-96'/></a>
       </section>
       <section className='bg-gray-200 py-12 px-4 border-t-red-500 border-solid border-t-2'>
         <h2 className='text-red-500 font-semibold text-2xl mb-4  text-center'>About us
@@ -37,6 +36,23 @@ export default function Home() {
           <InfoCard3 />
         </div>
 
+      </section>
+      <section className='bg-white py-8 px-4'>
+        <h1 className='text-red-500 text-2xl font-semibold text-center mb-4'>See our work</h1>
+          <div className='grid lg:grid-cols-3 gap-x-6 gap-y-4 text-white lg:px-12 lg:py-8'>
+            <div className='relative hover:shadow-2xl cursor-pointer hover:border-2 border-solid border-red-500 rounded-lg'>
+              <img src='https://i.postimg.cc/prQH8LbB/driveways.webp' className='rounded-lg'/>
+              <p className='absolute top-4 right-4 text-xl  font-semibold'>Driveways</p>
+            </div>
+            <div className='relative hover:shadow-2xl cursor-pointer hover:border-2 border-solid border-red-500 rounded-lg'>
+              <img src='https://i.postimg.cc/bNHnYxMZ/pool.jpg' className='rounded-lg'/>
+              <p className='absolute top-4 right-4 text-xl  font-semibold'>Pool decks</p>
+            </div>
+            <div className='relative hover:shadow-2xl cursor-pointer hover:border-2 border-solid border-red-500 rounded-lg'>
+              <img src='https://i.postimg.cc/L8BGp7D7/walk-way.jpg' className='rounded-lg h-full'/>
+              <p className='absolute top-4 right-4 text-xl  font-semibold'>Walk ways</p>
+            </div>
+          </div>
       </section>
       <Footer />
     </main>
