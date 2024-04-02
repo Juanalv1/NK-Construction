c800584bc483759d6492c16ebced88c673e94a72
Juan <elquesififa@gmail.com>
2024-04-02 17:18:13 -0500

    better footer

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -6,8 +6,6 @@ import { MdFacebook } from 'react-icons/md'
 const Footer = () => {
   return (
     <section className='flex justify-center items-center p-4 text-gray-700 py-8 flex-col bg-gray-200' >
-      <p>Made by <a href='https://portfolio-three-chi-15.vercel.app/' className='text-red-500 font-medium hover:underline'>Juan Alvarado</a></p>
-
       <div className='flex gap-x-4 items-center justify-center mt-4'>
         <a href='https://www.facebook.com/profile.php?id=100068440527013'>
           <MdFacebook className='text-blue-600 w-6 h-6'/>
@@ -17,6 +15,7 @@ const Footer = () => {
         </a>
         </div>
         <p className='mt-4 px-4 text-gray-500'>Nk construction LLC All rights reserved</p>
+        <p>Made by <a href='https://portfolio-three-chi-15.vercel.app/' className='text-red-500 font-medium hover:underline'>Juan Alvarado</a></p>
 
     </section>
   )
