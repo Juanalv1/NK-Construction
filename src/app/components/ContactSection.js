c2c5de250f1ad8cc4f13c5eee74036a30a93c385
Juan <elquesififa@gmail.com>
2024-04-05 09:01:42 -0500

    footer info added

--- src/app/components/ContactSection.js
+++ src/app/components/ContactSection.js
@@ -7,7 +7,7 @@ const ContactSection = () => {
       <div className='grid grid-cols-1 md:grid-cols-2 place-items-center'>
         <div className='flex flex-col gap-y-4 '>
           <p className='text-black text-lg'>If you are interested in our services or have any question, feel free to contact us via Phone or Email</p>
-          <p className='text-black text-xl'>Call us at: <span className='text-red-500 font-medium'>(012) 345678</span></p>
+          <p className='text-black text-xl'>Call us at: <span className='text-red-500 font-medium'>(704) 6005487</span></p>
           <p className='text-black  text-xl'>Email: <span className='text-red-500 font-medium'>nk92construction@gmail.com</span></p>
         </div>
         <img src='/contact.svg' className='w-96 h-80 flex lg:w-[450px] '/>
--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -5,17 +5,25 @@ import { MdFacebook } from 'react-icons/md'
 
 const Footer = () => {
   return (
-    <section className='flex justify-center items-center p-4 text-gray-700 py-8 flex-col bg-gray-200' >
-      <div className='flex gap-x-4 items-center justify-center mt-4'>
-        <a href='https://www.facebook.com/profile.php?id=100068440527013'>
-          <MdFacebook className='text-blue-600 w-6 h-6'/>
-        </a>
-        <a href='https://www.instagram.com/nk_construction.llc/'>
-          <LuInstagram className=' text-red-500 w-6 h-6'/>
-        </a>
-        </div>
+    <section className='grid grid-cols-1 lg:grid-cols-3 p-4 text-gray-700 py-8 flex-col bg-gray-200 md:flex-row lg:px-28' >
+      <div className='text-center'>
+        <p>Lenyn Torres </p>
+        <p>(704) 6005487</p>
+        <p>Dallas NC 28034</p>
+      </div>
+      <div className='text-center'>
         <p className='mt-4 px-4 text-gray-500'>Nk construction LLC All rights reserved</p>
-        <p>Made by <a href='https://portfolio-three-chi-15.vercel.app/' className='text-red-500 font-medium hover:underline'>Juan Alvarado</a></p>
+        <p className=''>Made by <a href='https://portfolio-three-chi-15.vercel.app/' className='text-red-500 font-medium hover:underline'>Juan Alvarado</a></p>
+      </div>
+      <div className='flex gap-x-4 items-center justify-center mt-4'>
+          <a href='https://www.facebook.com/profile.php?id=100068440527013'>
+            <MdFacebook className='text-blue-600 w-6 h-6'/>
+          </a>
+          <a href='https://www.instagram.com/nk_construction.llc/'>
+            <LuInstagram className=' text-red-500 w-6 h-6'/>
+          </a>
+      </div>
+
 
     </section>
   )
