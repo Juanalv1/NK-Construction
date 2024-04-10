6efd8d9a7c7dba0e5e0a554169c1277950c50381
Juan <elquesififa@gmail.com>
2024-04-10 08:16:53 -0500

    finished

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -37,6 +37,9 @@ const Navbar = () => {
         <nav className='flex flex-col p-4 bg-[#747474] w-2/3 fixed top-0 right-0 z-40 h-full' ref={menuRef}>
           <IoClose className='self-end w-6 h-6 text-[#f46868]' onClick={() => setIsMenuOpen(false)} />
           <ul className='flex flex-col gap-y-2 mt-12'>
+          <li className='hover:text-[#f46868]'><Link href={'/'} onClick={() => {
+              setIsMenuOpen(false)
+            }}>Home</Link></li>
             <li className='hover:text-[#f46868]'><Link href={'/works/driveways'} onClick={() => {
               setIsMenuOpen(false)
             }}>Driveways</Link></li>
@@ -56,13 +59,18 @@ const Navbar = () => {
               setIsMenuOpen(false)
             }}><Link href={'/works/concrete-pumping'} >Concrete Pumping</Link></li>
           </ul>
-          <button className='bg-[#f46868] p-2 rounded-lg mt-28 text-white'><a href='#contact'>Contact us</a></button>
+          <button className='bg-[#f46868] p-2 rounded-lg mt-28 text-white'><a href='https://nkconstructionllc.com/#contact' onClick={() => {
+              setIsMenuOpen(false)
+            }}>Contact us</a></button>
         </nav>
         <div className='w-full absolute top-0 right-0 bg-black opacity-50 z-10 h-full'></div>
       </>
       )}
       <nav className='hidden lg:flex items-center w-full justify-between'>
         <ul className='flex gap-y-2 items-center ml-8 gap-x-4 '>
+        <li className='hover:text-[#f46868]'><Link href={'/'} onClick={() => {
+              setIsMenuOpen(false)
+            }}>Home</Link></li>
         <li className='hover:text-[#f46868]'><Link href={'/works/driveways'}>Driveways</Link></li>
             <li className='hover:text-[#f46868]'><Link href={'/works/patios'}  onClick={() => {
               setIsMenuOpen(false)
@@ -80,7 +88,9 @@ const Navbar = () => {
               setIsMenuOpen(false)
             }}>Concrete pumping</Link></li>
         </ul>
-        <button className='bg-[#f46868] p-2 rounded-lg  text-white'><a href='/#contact'>Contact us</a></button>
+        <button className='bg-[#f46868] p-2 rounded-lg  text-white'><a href='https://nkconstructionllc.com/#contact' onClick={() => {
+              setIsMenuOpen(false)
+            }}>Contact us</a></button>
       </nav>
     </div>
   )
--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -1,22 +1,46 @@
 export default function sitemap() {
   return [
     {
-      url: 'https://acme.com',
+      url: 'https://nkconstructionllc.com/',
       lastModified: new Date(),
       changeFrequency: 'yearly',
       priority: 1,
     },
     {
-      url: 'https://acme.com/about',
+      url: 'https://nkconstructionllc.com/works/driveways',
       lastModified: new Date(),
       changeFrequency: 'monthly',
-      priority: 0.8,
+      priority: 1,
     },
     {
-      url: 'https://acme.com/blog',
+      url: 'https://nkconstructionllc.com/works/patios',
       lastModified: new Date(),
       changeFrequency: 'weekly',
-      priority: 0.5,
+      priority: 1,
+    },
+    {
+      url: 'https://nkconstructionllc.com/works/decorative-concrete',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 1,
+    },
+    {
+      url: 'https://nkconstructionllc.com/works/pool-decks',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 1,
+    },
+    {
+      url: 'https://nkconstructionllc.com/works/walk-ways',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 1,
+    },
+    {
+      url: 'https://nkconstructionllc.com/works/concrete-pumping',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 1,
     },
   ]
 }
