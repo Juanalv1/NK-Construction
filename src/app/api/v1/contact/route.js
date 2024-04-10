d2204a8583a6ef56270b9f1c1574d0f8dbefc9f6
Juan <elquesififa@gmail.com>
2024-04-10 14:37:05 -0500

    aa

--- src/app/api/v1/contact/route.js
+++ src/app/api/v1/contact/route.js
@@ -3,40 +3,42 @@ import nodemailer from 'nodemailer'
 
 export async function POST(req){
   const {email, name, phone_number, description} = await req.json()
+  console.log(process.env.HOST_PASSWORD)
+  console.log(process.env.HOST_USERNAME)
 
-  const transporter = nodemailer.createTransport({
-    host: "mail.privateemail.com",
-    port: 465,
-    auth: {
-        user: process.env.HOST_USERNAME,
-        pass: process.env.HOST_PASSWORD
-    }
-});
-  transporter.verify(function(error, success) {
-    if (error) {
-      console.error('Error al verificar la conexión con el servidor SMTP:', error);
-    } else {
-      console.log('Conexión exitosa con el servidor SMTP');
-    }
-  });
-  const mailOptions = {
-    from: 'lenynht@nkconstructionllc.com', // Remitente
-    to: 'lenynht@nkconstructionllc.com', // Destinatario
-    subject: 'Nuevo Contacto de proyecto', // Asunto
-    text: 
-      `Nombre: ${name}
-Telefono: ${phone_number}
-Correo: ${email}
-Descripcion: ${description}` // Contenido del correo
-  };
+//   const transporter = nodemailer.createTransport({
+//     host: "mail.privateemail.com",
+//     port: 465,
+//     auth: {
+//         user: process.env.HOST_USERNAME,
+//         pass: process.env.HOST_PASSWORD
+//     }
+// });
+//   transporter.verify(function(error, success) {
+//     if (error) {
+//       console.error('Error al verificar la conexión con el servidor SMTP:', error);
+//     } else {
+//       console.log('Conexión exitosa con el servidor SMTP');
+//     }
+//   });
+//   const mailOptions = {
+//     from: 'lenynht@nkconstructionllc.com', // Remitente
+//     to: 'lenynht@nkconstructionllc.com', // Destinatario
+//     subject: 'Nuevo Contacto de proyecto', // Asunto
+//     text: 
+//       `Nombre: ${name}
+// Telefono: ${phone_number}
+// Correo: ${email}
+// Descripcion: ${description}` // Contenido del correo
+//   };
 
-  transporter.sendMail(mailOptions, function(error, info) {
-    if (error) {
-      console.error('Error al enviar el correo electrónico:', error);
-    } else {
-      console.log('Correo electrónico enviado:', info.response);
-    }
-  });
+//   transporter.sendMail(mailOptions, function(error, info) {
+//     if (error) {
+//       console.error('Error al enviar el correo electrónico:', error);
+//     } else {
+//       console.log('Correo electrónico enviado:', info.response);
+//     }
+//   });
 
   return NextResponse.json({ message: "Hello World" })
 }
